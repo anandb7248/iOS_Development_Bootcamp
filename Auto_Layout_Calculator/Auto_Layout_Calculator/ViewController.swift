1cb0a6d9434a758ee838609a0f2f844e0bda0f6c
anandb7248 <anandb7248@gmail.com>
2019-02-10 14:51:01 -0800

    Completed auto layout section

---
+++ Auto_Layout_Calculator/Auto_Layout_Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto_Layout_Calculator
+//
+//  Created by Anand Batjargal on 2/10/19.
+//  Copyright © 2019 anandbatjargal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
